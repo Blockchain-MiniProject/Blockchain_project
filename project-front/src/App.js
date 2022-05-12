96d9990af11ab03b81effbdeee28d5abe8f0c73b
seungjackie <qkrtmdwo28@gmail.com>
2022-05-12 18:11:45 +0900

    asdf

--- project-front/src/App.js
+++ project-front/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import PostExample from './components/PostExample';
 import MiningButton from './components/MiningButton';
+import 'bootstrap/dist/css/bootstrap.min.css';          //bootstrap 가져온것
+
+
+
+// route
 
 function App() {
   return (
