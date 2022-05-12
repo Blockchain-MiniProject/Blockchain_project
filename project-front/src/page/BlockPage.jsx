38f18cde7711954226e120e71f736370b62973d9
seungjackie <qkrtmdwo28@gmail.com>
2022-05-12 18:11:45 +0900

    asdf

--- project-front/src/page/BlockPage.jsx
+++ project-front/src/page/BlockPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import BlockDetail from '../components/BlockDetail'
+import {Container , Row, Col}from 'react-bootstrap';
+
+
 
 const BlockPage = () => {
   return (
-    <>
-      <BlockDetail/>
-    </>
+    <div>BlockPage
+        <Container>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
+    </div>
   )
 }
 
