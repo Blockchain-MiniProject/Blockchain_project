adc8a159037dea26291a4367ac1fc277890d7a72
seungjackie <qkrtmdwo28@gmail.com>
2022-05-12 18:11:45 +0900

    asdf

--- project-front/src/components/BlockDetail.jsx
+++ project-front/src/components/BlockDetail.jsx
@@ -1,1 +1,14 @@
 // block page  블록디테일
+import React from 'react'
+
+// 1. 블록 데이터 가져오기
+// 2. hash, timestamp 
+
+
+const BlockDetail = () => {
+  return (
+    <div>BlockDetail</div>
+  )
+}
+
+export default BlockDetail
