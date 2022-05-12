f80c297b0a3a8f3457064ef3ce73597550623725
sungjinWi <rkdlem94@gmail.com>
2022-05-12 12:34:58 +0900

    Feat : get blocks 3500/blocks & console.log(ip)

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 export const addPeer = (req,res) => {
-    console.log(req.body);
-    // console.log(req.body)
-    // axios.post()
+    console.log(req.body.peer);
+    const ip = `ws://${req.body.peer}:3010`
+    console.log(ip)
+    // axios.post("http://localhost:3010/addPeer",ip)
     res.send("peer연결 완료");
 }
 
 
-export const getBlock = (req,res) => {
+export const getBlock = async (req,res) => {
     console.log("trying to getBlock");
     // blockchain에서 blocks[] 받아와서 DB에 넣고 프론트에 던져주자
-    res.send("db에 넣기 완료")
+    const result = await axios.get("http://localhost:6010/blocks")
+    res.send(result.data)
 }
