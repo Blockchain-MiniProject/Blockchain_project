a052555ed878b0c900f9bfc134804cb6961a2eaf
sungjinWi <rkdlem94@gmail.com>
2022-05-17 17:32:34 +0900

    db ing

--- back/SQL.sql
+++ back/SQL.sql
@@ -7,6 +7,17 @@ CREATE TABLE userinfo (
     `balance` INT(16) NOT NULL
 )DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+CREATE TABLE blocks (
+    `index` INT(11) NOT NULL ,
+    `data` VARCHAR(100) NOT NULL,
+    `timestamp` INT(16) NOT NULL,
+    `hash` VARCHAR(128) NOT NULL,
+    `previousHash` VARCHAR(128) NOT NULL,
+    `difficulty` INT(11) default 0 NOT NULL,
+    `nonce` INT(16) default 0 NOT NULL,
+    `miner` VARCHAR(256) NOT NULL
+) DEFAULT CHARSET utf8 COLLATE = utf8_general_ci;
+
 INSERT INTO userinfo(email,password,privatekey,address,balance) VALUES("sdf@nadfs","1234","4607ac1c52f58fed1a333805fa7023f8dad6d07e1168234dfcf61de829757e9a","04fb4120eea7c13858498e534aa21423357748e912fbc56481dd930843e9a769ce4d4b998dcca3db4ddecae3d283117785d017629837ebead98fcdf857e7e72313",0);
 
 SELECT * FROM userinfo;
