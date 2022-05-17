622510b19f242a73ed49bbbf5abcfe47255a4f01
HyejinLee11 <lhyejinll.ll@gmail.com>
2022-05-17 21:19:23 +0900

    코드정리

--- project-front/src/components/BlocksAll.jsx
+++ project-front/src/components/BlocksAll.jsx
@@ -4,38 +4,13 @@ import { Container, Table}from 'react-bootstrap';
 
 
 const BlockAll = ({allData}) => { 
-
-  // let {hash} = useParams();
   const navigate = useNavigate();
 
-  // 초기값 배열값으로 줘야 map 함수 , 배열로 하면 안된다.
-  // const [allData,setAllData] = useState([]);
-
-  // const getBlockData = async (data1) => {
-  //   let url = `http://localhost:3500/blocks`;
-  //   let response = await fetch(url)
-  //   let data = await response.json();
-  //   console.log("data123123"  , data);
-  //   console.log("block 123123" , allData );
-  //   setAllData(data)
-
-  //   console.log("data: ",data[5]);
-
-  //   const click = () => {
-  //     navigate(`/block/${data[5]}`)
-  //   }
-
-  // }
-
-
   const searchHash = (index) => {
-    // console.log(data)
     console.log(index)
-    // e.target.value
 
     // console.log("asdfasdf" , allData.index[])
     navigate(`/block/${index}`)
-    // navigate(`/block/1`)
   }
 
   // 참조롤 
@@ -75,68 +50,3 @@ const BlockAll = ({allData}) => {
 }
 
 export default BlockAll;
-
-
-// 클릭한 해쉬에 값에 id를 해쉬값으로 가져오기
-
- // const handleSearch = (event) => {
-  //   let value = event.target.value.toLowerCase();
-  //   let result = [];
-  //       console.log(value);
-  //       result = allData.filter((data) => {
-  //           return data.title.search(value) != -1;
-  //       });
-  //       setFilteredData(result);
-  // }
-
-  // const buttonAxiosget = () => {
-  //   console.log("요청하자")
-  //   axios.get("http://localhost:3010/mineBlock")
-  //   .then((res) => {
-  //     console.log(res.data);
-  //   })
-  //   .catch((err) => {
-  //     // error
-  //     console.log("error" , err)
-  //   })
-  // }
-
-
-  // const getBlock = async () => {
-  //   let searchQuery = qeury.get("q") || " ";
-  //   console.log("쿼리값은?",searchQuery)
-  //   // todo
-  //   let url = `http://localhost:3010/block?q= `; //${searchQuery}
-  //   let response =  await fetch(url);
-  //   let data = await response.json();
-  //   console.log(data)
-  //   setFilteredData(data)
-  // }
-
-  // 데이터 mine 블록 데이터 가져오는거 연습
-  // useEffect(() => {
-  //   axios.get(`http://localhost:3500/mineBlock`)
-  //   .then(response => {
-  //     console.log(response.data)
-  //     setAllData(response.data);
-  //     setFilteredData(response.data);
-  //   }).catch((error) => {
-  //       console.log('데이터 가져오기 : ' + error);
-  //   })
-  // }, []);
-
-{/* <div style={{ margin: '0 auto', marginTop: '10%' }}>
-            <label>Search:</label>
-            <input type="text" onChange={(event) =>handleSearch(event)} />
-<button onClick={buttonAxiosget} > axios.get </button> */ }
-                //         <div style={{padding:10}}>
-                //   {filteredData.map((value,index)=>{
-                //       return(
-                //           <div key={value.id}>
-                //               <div style={styles}>
-                //                   {value.title}
-                //               </div>
-                //           </div>
-                //       )
-                //   })}
-                // </div> 
