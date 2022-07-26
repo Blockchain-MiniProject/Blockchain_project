639bab979d2df33cb73baa5ad3dee3185bf6683e
sungjinWi <rkdlem94@gmail.com>
2022-07-26 16:04:23 +0900

    update : changed DB

--- back/db.js
+++ back/db.js
@@ -1,10 +1,10 @@
 import mysql from"mysql2/promise"; 
 
 const pool = mysql.createPool({
-    host:"49.50.172.129", //"localhost"
+    host:"34.64.165.60", //"localhost"
     port:3306,
-    user:"root",
-    password:"Sun13",
+    user:"sungjin",
+    password:"1234",
     database:"wallet"
 });
 
